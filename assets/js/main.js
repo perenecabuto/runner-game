bb1de51805ed98d140f6627e89af111539db0d35
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2015-09-06 13:04:54 -0300

    Use CANVAS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 (function() {
     'use strict';
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', new Game());
+    var game = new Phaser.Game(800, 400, Phaser.CANVAS, '', new Game());
 })();
 
